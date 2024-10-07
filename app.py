f2e4246a214d933dfed9dee37f6ed5faee6dc748
Tyler <penguincavalry@gmail.com>
2024-10-07 10:23:42 -0600

    Cause I can't help myself when there's the number sitting on the icon.

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('spell/<word>')
+def spell_word(word):
+    return render_template('spell_word.html', word=word)
+
 if __name__ == '__main__':
     app.run(debug=True)
