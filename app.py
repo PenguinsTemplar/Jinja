8abe046c8217e788a01a20f2ccfe13f11178ddf3
Tyler <penguincavalry@gmail.com>
2024-10-09 11:35:07 -0600

    adding Static folder for... stuff

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, render_template
+from random import choice, sample
+
 
 app = Flask(__name__)
 
@@ -6,9 +8,34 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('spell/<word>')
+@app.route('/form')
+def show_form():
+    return render_template("form.html")
+
+@app.route('/form-2')
+def show_form_2():
+    return render_template("form-2.html")
+
+COMPLIMENTS = ["Cool","Suave","Simple","Indiferent"]
+
+@app.route('/greet')
+def get_greeting():
+    username = request.args["username"]
+    nice_thing = choice(COMPLIMENTS)
+    return render_template("greet.html", username=username, compliment=nice_thing)
+
+@app.route('/greet-2')
+def get_greeting_2():
+    username = request.args["username"]
+    wants = request.args.get("wants_compliments")
+    nice_things = sample(COMPLIMENTS, 3)
+    return render_template("greet-2.html", username=username, wants_compliments=wants, compliments=nice_things)
+
+
+@app.route('/spell/<word>')
 def spell_word(word):
-    return render_template('spell_word.html', word=word)
+    caps_word = word.upper()
+    return render_template('spell_word.html', word = caps_word)
 
 if __name__ == '__main__':
     app.run(debug=True)
